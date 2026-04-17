2da987f360ee2fc87a72f4117269cb7e325b1f07
ng <glazer.nicholas@gmail.com>
2026-04-17 22:49:17 +0000

    teru 0.6.2

--- Formula/teru.rb
+++ Formula/teru.rb
@@ -1,8 +1,8 @@
 class Teru < Formula
   desc "AI-first terminal emulator, multiplexer, and tiling manager"
   homepage "https://github.com/nicholasglazer/teru"
-  url "https://github.com/nicholasglazer/teru/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "951b00c04a940390e57f05e9ab195337788948e2e918e09f7e74cf6621cf3087"
+  url "https://github.com/nicholasglazer/teru/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "73fce026c331e34c94ec75ddb1e620b0f06baee9f6c06a4709c345e29d8ccacd"
   license "MIT"
   head "https://github.com/nicholasglazer/teru.git", branch: "main"
 
